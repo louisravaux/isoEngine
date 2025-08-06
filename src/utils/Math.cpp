ceb3772778ba5517bb3a54a0d5160cef5b5d88c3
Louis Ravaux <louis.ravaux@imt-atlantique.net>
2025-08-06 19:57:13 +0200

    fix: use floor instead of round int Math

--- src/utils/Math.cpp
+++ src/utils/Math.cpp
@@ -9,18 +9,18 @@ void Math::toScreenCoordinates(int w, int h, int gridX, int gridY, int& screenX,
 }
 
 void Math::toGridCoordinates(int w, int h, int screenX, int screenY, int& gridX, int& gridY) {    
-    float a = i_x * 0.5 * w;
-    float b = j_x * 0.5 * w;
-    float c = i_y * 0.5 * h;
-    float d = j_y * 0.5 * h;
+    float a = i_x * 0.5f * w;
+    float b = j_x * 0.5f * w;
+    float c = i_y * 0.5f * h;
+    float d = j_y * 0.5f * h;
         
     invertMatrix(a, b, c, d);
         
     float rawX = a * screenX + b * screenY;
     float rawY = c * screenX + d * screenY;
         
-    gridX = static_cast<int>(std::round(rawX));
-    gridY = static_cast<int>(std::round(rawY));
+    gridX = static_cast<int>(std::floor(rawX));
+    gridY = static_cast<int>(std::floor(rawY));
     
 }
 
